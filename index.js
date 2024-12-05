129ab8e19e3a12e45b59069e69c16c2926ccb974
Christian <ccastro@corpuscorp.com>
2024-12-05 01:12:28 -0300

    Creando Api Restful

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+
+
+
+
+app.get('/',(req, res)=>{
+    res.send('Hola mundo')   
+})
+
+const port = process.env.PORT || 3000;
+app.listen (port, ()=>{
+    console.log(`Servidor ejecutando con Express desde el puerto ${port}`)
+})
